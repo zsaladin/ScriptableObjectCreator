3c6cf609050a87cd67a3914d2aad89c66ca02020
Dae Hee Kim <zsaladinz@gmail.com>
2017-04-16 22:16:15 +0900

    Added ScriptableObjectCreator

---
+++ ScriptableObjectCreator/Assets/Editor/ScriptableObjectCreator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class ScriptableObjectCreator
+{
+	private static string WARNING_TITLE = "Warning";
+	private const string WARNING_MESSAGE = "Select ScriptableObject script";
+	private const string WARNING_OK = "OK";
+
+	[MenuItem("Assets/Create/Scriptable Object")]
+    public static void Init()
+    {
+        MonoScript monoScript = Selection.activeObject as MonoScript;
+		if (monoScript == null)
+		{
+			EditorUtility.DisplayDialog(WARNING_TITLE, WARNING_MESSAGE, WARNING_OK);
+			return;
+		}
+
+		Type type = monoScript.GetClass();
+		if (type.IsSubclassOf(typeof(ScriptableObject)) == false)
+		{
+			EditorUtility.DisplayDialog(WARNING_TITLE, WARNING_MESSAGE, WARNING_OK);
+			return;
+		}
+
+		ScriptableObject scriptableObjectInstance = ScriptableObject.CreateInstance(type);
+		AssetDatabase.CreateAsset(scriptableObjectInstance, Path.ChangeExtension(AssetDatabase.GetAssetPath(monoScript), "asset"));
+		AssetDatabase.SaveAssets();
+
+		EditorUtility.FocusProjectWindow();
+		Selection.activeObject = scriptableObjectInstance;
+    }
+}
